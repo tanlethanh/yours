6fda1adc426fd08d46fed7fafb216f6827995626
Tan Le <tanlethanh8266@gmail.com>
2023-03-04 18:02:58 +0700

    Add some stuff to serve taiwind for other folders

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,8 @@ module.exports = {
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
     './app/**/*.{js,ts,jsx,tsx}',
+    './layouts/**/*.{js,ts,jsx,tsx}',
+    './utils/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
     extend: {},
