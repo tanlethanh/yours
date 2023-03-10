b1d668cf17424be1479a7b7de05242f5b7c52cd5
Tan Le <tanlethanh8266@gmail.com>
2023-03-10 21:40:26 +0700

    Add get access token api

--- src/routes/api/v1.ts
+++ src/routes/api/v1.ts
@@ -1,11 +1,10 @@
 import express, { Router } from 'express';
 import SampleController from '../../controllers/SampleController';
+import NotionAuthController from '../../controllers/NotionAuthController';
 
 const apiV1: Router = express.Router()
 
-apiV1.route('/sample')
-   .get(SampleController.getSample)
-
-apiV1.route('/hello')
+apiV1.route('/notion-auth')
+   .post(NotionAuthController.postCode)
 
 export default apiV1
