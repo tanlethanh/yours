bdec5971db249c7b6f7d936d057b59a4befc62e9
Tan Le <tanlethanh8266@gmail.com>
2023-03-10 21:39:09 +0700

    Refactor Locals provider and adding some fields

--- src/providers/Locals.ts
+++ src/providers/Locals.ts
@@ -9,43 +9,14 @@ class Locals {
 	 */
 	public static config(): any {
 		dotenv.config({ path: path.join(__dirname, '../../.env') });
-		const nodeEnv = process.env.NODE_ENV || "development"
-		const url = process.env.APP_URL || `http://localhost:${process.env.PORT}`;
-		const port = process.env.PORT || 4040;
-		const maxUploadLimit = process.env.APP_MAX_UPLOAD_LIMIT || '50mb';
-		const maxParameterLimit = process.env.APP_MAX_PARAMETER_LIMIT || '50mb';
-
-		const name = process.env.APP_NAME || 'NodeTS Dashboard';
-		const keywords = process.env.APP_KEYWORDS || 'somethings';
-		const year = (new Date()).getFullYear();
-		const copyright = `Copyright ${year} ${name} | All Rights Reserved`;
-
-		const isCORSEnabled = process.env.CORS_ENABLED || true;
-		const jwtExpiresIn = process.env.JWT_EXPIRES_IN || 3;
-		const apiPrefix = process.env.API_PREFIX || 'api';
-
-		const logDays = process.env.LOG_DAYS || 10;
 		
-		const automateDuration = process.env.AUTOMATE_DURATION
-		const automateDurationInSeconds = Number(process.env.AUTOMATE_DURATION_IN_SECONDS)
-		const unlockPercent = Number(process.env.UNLOCK_PERCENT)
 
 		return {
-			apiPrefix,
-			copyright,
-			isCORSEnabled,
-			jwtExpiresIn,
-			keywords,
-			logDays,
-			maxUploadLimit,
-			maxParameterLimit,
-			name,
-			port,
-			url,
-			nodeEnv,
-			automateDuration,
-			automateDurationInSeconds,
-			unlockPercent
+			PORT: process.env.PORT,
+			MAX_UPLOAD_LIMIT: process.env.MAX_UPLOAD_LIMIT,
+			NOTION_OAUTH_ID: process.env.NOTION_OAUTH_ID,
+			NOTION_OAUTH_SECRET: process.env.NOTION_OAUTH_SECRET,
+			NOTION_TOKEN: process.env.NOTION_TOKEN
 		};
 	}
 
