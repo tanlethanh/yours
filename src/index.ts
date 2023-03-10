b17a314fe2ba51d4f8a08764c04e49982222dffa
Tan Le <tanlethanh8266@gmail.com>
2023-03-10 21:42:30 +0700

    Fix port

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ const expressApp = new ExpressApp()
 const server = http.createServer(expressApp.app)
 
 // Start the server on the specified port
-const PORT = Locals.config().port
+const PORT = Locals.config().PORT
 server.listen(PORT, () => {
     return console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
 }).on('error', (_error: Error) => {
