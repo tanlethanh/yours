7ce67f9765dc1b75bef4183f39172d85d9860c4f
Tan Le <tanlethanh8266@gmail.com>
2023-03-21 01:04:19 +0700

    Change config for default user

--- src/providers/Locals.ts
+++ src/providers/Locals.ts
@@ -1,7 +1,7 @@
 import { Application } from "express";
 import * as path from "path";
 import * as dotenv from "dotenv";
-import * as url from 'url'
+import * as url from "url";
 
 const __filename = url.fileURLToPath(import.meta.url);
 
@@ -19,8 +19,9 @@ class Locals {
 
         return {
             NODE_ENV: process.env.NODE_ENV,
-            DEFAULT_USER_FOR_DEVELOPMENT:
-                process.env.DEFAULT_USER_FOR_DEVELOPMENT,
+            USE_DEFAULT_USER: process.env.USE_DEFAULT_USER == "true",
+            DEFAULT_USER_EMAIL: process.env.DEFAULT_USER_EMAIL,
+            DEFAULT_USER_FIREBASE_UID: process.env.DEFAULT_USER_FIREBASE_UID,
             PORT: process.env.PORT,
             MAX_UPLOAD_LIMIT: process.env.MAX_UPLOAD_LIMIT,
             NOTION_OAUTH_ID: process.env.NOTION_OAUTH_ID,
@@ -29,7 +30,6 @@ class Locals {
             MONGOOSE_URI: process.env.MONGOOSE_URI,
             FIREBASE_REFRESH_TOKEN: process.env.FIREBASE_REFRESH_TOKEN,
             FIREBASE_DB_URL: process.env.FIREBASE_DB_URL,
-            USE_DEFAULT_USER: process.env.USE_DEFAULT_USER == "true"
         };
     }
 
