ac029f9c7d6ff395833f017fb4b2db352e29201d
Tan Le <tanlethanh8266@gmail.com>
2023-03-21 00:56:45 +0700

    Add middleware into routes, refactor controller to meet middlewares

--- src/routes/api/v1.ts
+++ src/routes/api/v1.ts
@@ -1,13 +1,16 @@
-import express, { Router } from 'express';
-import SampleController from '../../controllers/SampleController.js';
-import NotionController from '../../controllers/NotionController.js';
+import express, { Router } from "express";
+import SampleController from "../../controllers/SampleController.js";
+import NotionController from "../../controllers/NotionController.js";
+import Auth from "../../middlewares/Auth.js";
 
-const apiV1: Router = express.Router()
+const apiV1: Router = express.Router();
 
-apiV1.route('/notion/auth')
-   .post(NotionController.postAuthCode)
+apiV1
+    .route("/notion/auth")
+    .post(Auth.userFilter, NotionController.postAuthCode as any);
 
-apiV1.route('/notion/data/sync')
-   .post(NotionController.syncDataByUserId)
+apiV1
+    .route("/notion/data/sync")
+    .post(Auth.userFilter, NotionController.syncDataByUserId as any);
 
-export default apiV1
+export default apiV1;
