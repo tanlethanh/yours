186a9d8d7a42e741e70ad829817bedc0f1bf26b1
Tan Le <tanlethanh8266@gmail.com>
2023-03-21 13:18:24 +0700

    Wrap notion-redirect page by withAuth

--- pages/notion-redirect.tsx
+++ pages/notion-redirect.tsx
@@ -1,27 +1,23 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef } from 'react'
 import { useRouter } from 'next/router'
-import axios from 'axios'
+// import axios from 'axios'
+import { apiAxios } from '../utils/axiosConfig'
+import { withAuth } from '../components/withAuth'
 
 function RedirectPage() {
     const router = useRouter()
     const { query } = router
+    const dataFetchedRef = useRef(false)
 
-    const base64encodedData = new Buffer(
-        process.env.NEXT_PUBLIC_OAUTH_CLIENT_ID +
-            ':' +
-            process.env.NEXT_PUBLIC_OAUTH_CLIENT_ID
-    ).toString('base64')
+    const fetchData = async () => {
+        console.log('Query string: ', query.code)
+        const res = await apiAxios.post(`notion/auth?code=${query.code}`)
+        console.log(res)
+    }
 
     useEffect(() => {
-        if (!router.isReady) return
-
-        const fetchData = async () => {
-            console.log('Query string: ', query.code)
-            const res = await axios.post(
-                `api/v1/notion/auth?code=${query.code}`
-            )
-            console.log(res)
-        }
+        if (!router.isReady || dataFetchedRef.current) return
+        dataFetchedRef.current = true
         fetchData()
     }, [router.isReady])
 
@@ -33,4 +29,4 @@ function RedirectPage() {
     )
 }
 
-export default RedirectPage
+export default withAuth(RedirectPage)
