34bf61ca59a50f2c77f7cfe41606fc419fd92031
Tan Le <tanlethanh8266@gmail.com>
2023-03-23 20:19:05 +0700

    Add user api

--- src/routes/api/v1.ts
+++ src/routes/api/v1.ts
@@ -3,6 +3,7 @@ import SampleController from "../../controllers/SampleController.js";
 import NotionController from "../../controllers/NotionController.js";
 import Auth from "../../middlewares/Auth.js";
 import TestsController from "../../controllers/TestsController.js";
+import UsersController from "../../controllers/UsersController.js";
 
 const apiV1: Router = express.Router();
 
@@ -17,5 +18,9 @@ apiV1
 apiV1
     .route("/tests/new-test")
     .get(Auth.userFilter, TestsController.getNewTest as any);
+
+apiV1
+    .route("/users/notion-connect")
+    .get(Auth.userFilter, UsersController.isUserConnectToNotion as any);
 
 export default apiV1;
