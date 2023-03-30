24312dc56b5b0a12bd3af54961320d0bb90861b8
Tan Le <tanlethanh8266@gmail.com>
2023-03-30 20:14:35 +0700

    Add fail animated element after done test

--- pages/tests/[testId]/[questionId].tsx
+++ pages/tests/[testId]/[questionId].tsx
@@ -7,12 +7,14 @@ import { apiAxios } from '../../../utils/axiosConfig';
 import { DataTestsContext } from '../../../store/DataTestsContext';
 import CongratsElement from '../../../components/animatedElements/congratsElement';
 import Link from 'next/link';
+import FailElement from '../../../components/animatedElements/failElement';
 
 function TestPage() {
     const router = useRouter();
     const { testId, questionId } = router.query;
     const [isFinal, setIsFinal] = useState(false);
     const context = useContext(DataTestsContext);
+    const isFail = (context.countCorrect * 1.0) / context.testsDatas.length < 0.7;
 
     return (
         <TestLayout>
@@ -24,13 +26,15 @@ function TestPage() {
                 setFinal={setIsFinal}
             />
             {isFinal && (
-                <div className="flex flex-row space-x-4">
-                    <div className="flex flex-col space-y-16 pt-10">
+                <div
+                    className={
+                        'flex flex-wrap justify-center items-center ' + (isFail ? 'flex-row-reverse' : 'flex-row')
+                    }
+                >
+                    <div className="flex flex-col space-y-16 pt-10 w-[400px] justify-center items-center">
                         <div>
                             <h1 className="text-3xl font-medium mb-4">Kết quả</h1>
-                            <p className="text-8xl">
-                                {`${context.countCorrect} / ${context.testsDatas.length}`}
-                            </p>
+                            <p className="text-8xl">{`${context.countCorrect} / ${context.testsDatas.length}`}</p>
                         </div>
                         <Link href={'/dashboard'} className="w-fit px-8 py-3 border border-gray-300 rounded-md">
                             {' '}
@@ -38,7 +42,11 @@ function TestPage() {
                         </Link>
                     </div>
                     <div>
-                        <CongratsElement width={400} height={400}></CongratsElement>
+                        {isFail ? (
+                            <FailElement width={400} height={400}></FailElement>
+                        ) : (
+                            <CongratsElement width={400} height={400}></CongratsElement>
+                        )}
                     </div>
                 </div>
             )}
