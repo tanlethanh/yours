09298c785aec69acc15a53444e6fc087c1430f96
Tan Le <tanlethanh8266@gmail.com>
2023-05-27 11:03:27 +0700

    Add base Lottie components with default option

--- apps/web/src/components/lottie/index.tsx
+++ apps/web/src/components/lottie/index.tsx
@@ -1,3 +1,34 @@
+import { type FC } from 'react';
+import PureLottie, { type Options } from 'react-lottie';
+
+interface Props {
+	width?: number;
+	height?: number;
+	animationData: unknown;
+	options?: Options;
+}
+
+export const Lottie: FC<Props> = ({
+	height = 130,
+	width = 130,
+	animationData,
+	options = {},
+}) => {
+	const defaultOptions = {
+		loop: true,
+		autoplay: true,
+		animationData: animationData,
+		renderer: 'svg',
+	};
+	return (
+		<PureLottie
+			options={{ ...defaultOptions, ...options }}
+			height={height}
+			width={width}
+		/>
+	);
+};
+
 export * from './Congrats';
 export * from './Fail';
 export * from './HutBanner';
