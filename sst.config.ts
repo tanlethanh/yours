7cc9eb123c8eb2ed2e4744f42fb72349204b6feb
Tan Le <tanlethanh8266@gmail.com>
2023-06-20 05:47:09 +0700

    final config to deploy next app

--- sst.config.ts
+++ sst.config.ts
@@ -1,26 +1,25 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { SSTConfig } from 'sst';
 import { Bucket, NextjsSite } from 'sst/constructs';
 
 export default {
-    config(_input) {
-        return {
-            name: 'yours-web',
-            region: 'ap-northeast-1',
-        };
-    },
-    stacks(app) {
-        app.stack(function Site({ stack }) {
-            const bucket = new Bucket(stack, 'public');
+	config(_input) {
+		return {
+			name: 'yours-web',
+			region: 'ap-northeast-1',
+		};
+	},
+	stacks(app) {
+		app.stack(function Site({ stack }) {
+			const bucket = new Bucket(stack, 'public');
 
-            const site = new NextjsSite(stack as never, 'site', {
-                path: 'apps/web',
-                bind: [bucket],
-            });
+			const site = new NextjsSite(stack as never, 'site', {
+				path: 'apps/web',
+				bind: [bucket],
+			});
 
-            stack.addOutputs({
-                SiteUrl: site.url,
-            });
-        });
-    },
+			stack.addOutputs({
+				SiteUrl: site.url,
+			});
+		});
+	},
 } satisfies SSTConfig;
